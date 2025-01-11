f951df176dc0281ee23e4e935759dea7600f91fa
Tulsi Narayanan <tulsinarayanan33@gmail.com>
2025-01-11 23:47:23 +0530

    Seventh Commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,28 @@
+import {useRef, useState} from "react";
+
 const Hero = () => {
+  const [currentIndex, setCurrentIndex] = useState(1);
+  const [hasClicked, setHasClicked] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedVideos, setLoadedVideos] = useState(0);
+
+  const totalVideos = 4;
+
+  const handleMiniVideoClick = () => {
+    setHasClicked(true);
+
+    setCurrentIndex((prevIndex) => prevIndex + 1);
+  };
+  const nextVideoRef = useRef(null);
+
   return (
     <div className="relative h-dvh w-screen overflow-x-hidden">
-      <div className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75">
+      <div id="video-frame" className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75">
         <div>
-          <div className="mask-clip-path">
-            
+          <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
+            <div onClick={handleMiniVideoClick}>
+              MiniVideoPlayer
+            </div>
           </div>
         </div>
       </div>

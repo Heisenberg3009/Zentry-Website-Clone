37accba73d4eb87e7131100acca5999c221eab1f
Tulsi Narayanan <tulsinarayanan33@gmail.com>
2025-01-12 19:47:04 +0530

    Twenty Sixth Commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,29 @@
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
+
 const About = () => {
+  useGSAP(() => {
+    const clipAnimation = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#clip",
+        start: "center center",
+        end: "+=800 center",
+        scrub: 0.5,
+        pin: true,
+        pinSpacing: true,
+      },
+    });
+
+    clipAnimation.to(".mask-clip-path", {
+      width: "100vw",
+      height: "100vh",
+      borderRadius: 0,
+    });
+  });
+
   return (
     <div id="about" className="min-h-screen w-screen">
       <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
@@ -14,7 +39,13 @@ const About = () => {
         </div>
       </div>
       <div className="h-dvh w-screen" id="clip">
-        <div className="mask-clip-path about-image"></div>
+        <div className="mask-clip-path about-image">
+          <img
+            src="img/about.webp"
+            alt="Background"
+            className="absolute left-0 top-0 size-full object-cover"
+          />
+        </div>
       </div>
     </div>
   );

a82be05db29d60776cf72fa939f848a79b146a5c
Tulsi Narayanan <tulsinarayanan33@gmail.com>
2025-01-14 18:35:13 +0530

    Twenty Eighth Commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,7 @@
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/all";
+import AnimatedTitle from "./AnimatedTitle.jsx";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -30,11 +31,12 @@ const About = () => {
         <h2 className="font-general text-sm uppercase md:text-[10px]">
           Welcome to Zentry
         </h2>
-        <div className="mt-5 text-center text-4xl uppercase reading-[0.8] md:text-[6rem]">
-          Disc<b>o</b>ver the world's <br /> l<b>a</b>rgest shared adventure
-        </div>
+        <AnimatedTitle
+          title="Disc<b>o</b>ver the world's <br /> l<b>a</b>rgest shared adventure"
+          containerClass="mt-5 !text-black text-center"
+        />
         <div className="about-subtext">
-          <p>The Game of Games begins! You're life, now an epic MMORPG.</p>
+          <p>The Game of Games begins! Your life, now an epic MMORPG.</p>
           <p>Zentry unites every player from countless games and platforms.</p>
         </div>
       </div>
